893bd4a5864542e7445b66b0b6f1c62855f8ff0e
Kanishk Kumar <kumar88kanishk@gmail.com>
2021-09-16 22:17:04 +0530

    Tests updated

--- test/parking_lot/core_test.clj
+++ test/parking_lot/core_test.clj
@@ -4,20 +4,28 @@
 
 (deftest create-parking-lot-test
   (testing "Create Parking Lot"
-    (is (= {:0 [nil]} (pl/create-parking-lot 1 1)))))
+    (is (= {:floors '((nil))
+            :veh-size {:cycle 1
+                       :car 2
+                       :bus 4}} (pl/create-parking-lot 1 1 {:floors []
+                                                            :veh-size {:cycle 1
+                                                                       :car 2
+                                                                       :bus 4}})))))
 
-(deftest get-veh-weight-test
-  (testing "Get Vehicle Weight based on type"
-    (is (= 2 (pl/get-veh-weight "car")))))
 
-(deftest search-parking-floor-test
-  (testing "Search available parking floor"
-    (let [parking-lot (pl/create-parking-lot 1 1)
-          floor (pl/search-parking-floor 1 parking-lot)]
-      (is (= :0 floor)))))
+(deftest park-test
+  (testing "Park Vehicle"
+    (is (= [true {:floors '(({:num "UP3213123"} {:num "UP3213123"})), :veh-size {:cycle 1, :car 2, :bus 4}}]
+           (pl/park {:floors '((nil nil))
+                     :veh-size {:cycle 1
+                                :car 2
+                                :bus 4}} "car" "UP3213123")))))
 
-(deftest new-parking-state-test
-  (testing "Park vehicle and update state"
-    (let [parking-lot-state (pl/create-parking-lot 1 2)
-          park (pl/new-parking-state 2 "UP32ABC" parking-lot-state)]
-      (is (= {:0 ["UP32ABC" "UP32ABC"]} park)))))
+
+(comment
+  (pl/create-parking-lot 1 1 {:floors []
+                              :veh-size {:cycle 1
+                                         :car 2
+                                         :bus 4}})
+  (seq (seq nil))
+  (clojure.test/run-tests))
